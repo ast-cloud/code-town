83a05159780832f3bf57d5ebf787b1fd76e2718c
Aastik Yadav <aastikyadav1997.ay@gmail.com>
2023-10-23 20:42:47 +0530

    Created about us page stub code

---
+++ apps/users/src/pages/aboutus.tsx
@@ -0,0 +1,16 @@
+import { Typography, Card, TextField, Button } from '@mui/material';
+import {themeColors} from 'ui';
+
+
+
+
+export default function AboutUs(){
+
+    return (
+        <div>
+            
+            About Us
+
+        </div>
+    )
+}
